fd22f9d6fa60f3679ff7228f08c1e70b7a0b6a49
Omar Garcia <ojgarcia31@gmail.com>
2023-06-02 17:05:11 -0500

    Refactored http methods to data-gateway service

--- StudentFrontEnd/src/app/register/register.component.ts
+++ StudentFrontEnd/src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { AccountRegistration } from '../services/AccountRegistration';
+import { DataGatewayService } from '../services/data-gateway.service';
 
 @Component({
   selector: 'app-register',
@@ -7,15 +9,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  firstname: string = "";
-  lastname: string = "";
-  email: string = ""; 
-  password: string = "";
+  firstname!: string;
+  lastname!: string;
+  email!: string; 
+  password!: string;
   role: string="student";
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, private dataService: DataGatewayService){}
 
-  save(){
+  onSubmit() {
+    if (!this.firstname || !this.lastname || !this.email || !this.password) {
+      alert('Please enter required information.')
+      return;
+    }
+
+    const accountRegistration: AccountRegistration = {
+      firstname: this.firstname,
+      lastname: this.lastname,
+      email: this.email,
+      password: this.password,
+      role: this.role
+    };
+
+    console.log(accountRegistration);
+
+    let resultData: any =
+      this.dataService.getAccountRegistered(accountRegistration).subscribe((accountRegistration) => resultData);
+    
+    console.log(resultData);
+    alert("student registered successfully");
+  }
+
+}
+
+/*
+save(){
     let bodyData = {
       "userFirstName" : this.firstname,
       "userLastName" : this.lastname,
@@ -29,6 +57,4 @@ export class RegisterComponent {
       alert("student registered successfully");
     });
   }
-
-
-}
+*/
